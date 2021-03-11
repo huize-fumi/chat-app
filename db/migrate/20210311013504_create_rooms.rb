fbf7f55aa1c7dbc4e4f7a4f0c5ae344cf16976da
huize-fumi <arin0ma2@yahoo.co.jp>
2021-03-11 11:04:36 +0900

    新規作成画面の表示

--- db/migrate/20210311013504_create_rooms.rb
+++ db/migrate/20210311013504_create_rooms.rb
@@ -1,8 +1,7 @@
-class CreateRoooms < ActiveRecord::Migration[6.0]
+class CreateRooms < ActiveRecord::Migration[6.0]
   def change
-    create_table :roooms do |t|
+    create_table :rooms do |t|
       t.string :name, null: false
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_003259) do
+ActiveRecord::Schema.define(version: 2021_03_11_013504) do
 
-  create_table "roooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
