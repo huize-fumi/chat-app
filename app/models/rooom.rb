fd7ceffea2dd5f17e665f832d25d4451d801e94a
huize-fumi <arin0ma2@yahoo.co.jp>
2021-03-11 10:10:25 +0900

    roomsテーブルの作成

---
+++ app/models/rooom.rb
@@ -0,0 +1,2 @@
+class Rooom < ApplicationRecord
+end
---
+++ db/migrate/20210311003259_create_roooms.rb
@@ -0,0 +1,9 @@
+class CreateRoooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :roooms do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
